a82d1690976e6c3367386a82dfe8daaba5115384
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-03-14 10:51:42 -0400

    Clean up ProductDAO only; No need for create Instance in AppDatabase (has AppDatabaseSingleton)

--- app/src/main/java/com/cs4520/assignment4/repository/AppDatabase.kt
+++ app/src/main/java/com/cs4520/assignment4/repository/AppDatabase.kt
@@ -1,26 +1,10 @@
 package com.cs4520.assignment4.repository
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.cs4520.assignment4.models.ProductEntity
 
 @Database(entities = [ProductEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
-
-    companion object {
-        private var INSTANCE: AppDatabase? = null
-        fun getDatabase(context: Context): AppDatabase {
-            if (INSTANCE == null) {
-                synchronized(this) {
-                    INSTANCE =
-                        Room.databaseBuilder(context,AppDatabase::class.java, "contact_database")
-                            .build()
-                }
-            }
-            return INSTANCE!!
-        }
-    }
 }
