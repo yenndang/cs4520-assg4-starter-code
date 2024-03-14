c79edb589fd8bb1a5f2553f7aef8b7fd67f10416
yenlnldang@gmail.com <yenlnldang@gmail.com>
2024-03-14 10:36:39 -0400

    Clean up ProductDAO; Create instance function for AppDatabase

--- app/src/main/java/com/cs4520/assignment4/repository/ProductDAO.kt
+++ app/src/main/java/com/cs4520/assignment4/repository/ProductDAO.kt
@@ -5,7 +5,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Transaction
 import com.cs4520.assignment4.models.ProductEntity
 
 @Dao
@@ -15,13 +14,4 @@ interface ProductDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(products: List<ProductEntity>)
-
-    @Query("DELETE FROM products")
-    suspend fun deleteAllProducts()
-
-//    @Transaction // all operations inside are run atomically
-//    suspend fun clearAndCacheProducts(products: List<ProductEntity>) {
-//        deleteAllProducts()
-//        insertAll(products)
-//    }
 }
